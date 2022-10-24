597418ca8d0fb8d700a14d039c86d3535b5118fd
Zulhilmi Ramli <zulhilmi.ramli@outlook.my>
2022-10-24 10:26:30 +0800

    Working script now

--- taskwarrior-sync.py
+++ taskwarrior-sync.py
@@ -8,21 +8,22 @@ The sequence for syncing are:
 4. Push
 '''
 
-from turtle import update
 import requests
 import json
 import os
 import subprocess
 from pathlib import Path
+import argparse
 
+## Check for required variable. Since most of it sensitive, user must set it up
 try:
-    token=os.environ['TASK_SYNC_TOKEN']
+    GIST_ACCESS_TOKEN=os.environ['TASK_SYNC_TOKEN']
 except:
     print("Error: Export PLEASE_SYNC_TOKEN with your Gist access token")
     exit()
 
 try:
-    gist_id=os.environ['TASK_GIST_ID']
+    GIST_ID=os.environ['TASK_GIST_ID']
 except:
     print("Error: Export PLEASE_GIST_ID with your Gist ID")
     exit()
@@ -41,8 +42,10 @@ PACKED_CONFIG = "taskConfig.tar.gz"
 PACKED_CONFIG_DECRYPTED = "taskConfig_dec.tar.gz"
 GIST_FILENAME = "task-sync.json"
 
+
 ## Compress file and folder via tar -czf task.tar.gz TASK_CONFIG TASK_FOLDER
 def packConfig():
+    print("Packing config into tar...")
     r = subprocess.run(args=[ "tar",
                               "-czf",
                               TMP + PACKED_CONFIG,
@@ -51,10 +54,22 @@ def packConfig():
                               ],
                        universal_newlines = True,
                        stdout = subprocess.PIPE )
-    print(r.stdout)
+    # print(r.stdout)
+
+def unpackConfig():
+    print("Unpacking tar config...")
+    r = subprocess.run(args=[ "tar",
+                              "-xzf",
+                              TMP + PACKED_CONFIG_DECRYPTED,
+                              "-C",
+                              "/"
+                              ],
+                       universal_newlines = True,
+                       stdout = subprocess.PIPE )
 
 ## Encrypt the package
 def encryptConfig():
+    print("Encrypting config...")
     r = subprocess.run(args=[ "gpg",
                           "--recipient",
                           PGP_ID,
@@ -65,10 +80,11 @@ def encryptConfig():
                           TMP + PACKED_CONFIG ],
                   universal_newlines = True,
                   stdout = subprocess.PIPE )
-    print(r.stdout)
+    # print(r.stdout)
     return r.stdout
 
 def decryptConfig( input ):
+    print("Decrypting config...")
     # decrypt the data and overwrite local data
     echo_r = subprocess.Popen(('echo', input), stdout=subprocess.PIPE)
     decrypt_r = subprocess.Popen(('gpg', '--decrypt'), stdin=echo_r.stdout, stdout=subprocess.PIPE)
@@ -80,6 +96,7 @@ def decryptConfig( input ):
     # print(decrypt_r.stdout)s
 
 def packJson( input ):
+    print("Packing config to JSON format...")
     # Build JSON object
     packedData = {}
     packedData['modified'] = 0
@@ -87,86 +104,50 @@ def packJson( input ):
 
     # Build JSON Object
     jData = json.dumps(packedData)
-    print( "Packed JSON: " + jData)
+    # print( "Packed JSON: " + jData)
 
     return jData
 
 ## Get gist
 def getGist():
-    headers = {'Authorization': f'token {token}'}
-    r = requests.get('https://api.github.com/gists/' + gist_id, headers=headers) 
+    print("Pulling config from GIST...")
+    headers = {'Authorization': f'token {GIST_ACCESS_TOKEN}'}
+    r = requests.get('https://api.github.com/gists/' + GIST_ID, headers=headers) 
     # print(r.json())
     return r.json()
 
 ## Update gist
 def updateGist( content ):
+    print("Pushing config to GIST...")
     # content=open(filename, 'r').read()
-    headers = {'Authorization': f'token {token}'}
-    r = requests.patch('https://api.github.com/gists/' + gist_id, data=json.dumps({'files':{GIST_FILENAME:{"content":content}}}),headers=headers) 
+    headers = {'Authorization': f'token {GIST_ACCESS_TOKEN}'}
+    r = requests.patch('https://api.github.com/gists/' + GIST_ID, data=json.dumps({'files':{GIST_FILENAME:{"content":content}}}),headers=headers) 
     #print(r.json())
 
-packConfig()
 
-data = encryptConfig()
-packedJson = packJson( data )
+parser = argparse.ArgumentParser(description='Simple Task Warrior task sync. Uses Github as storage and PGP as encryption')
+parser.add_argument('--push', action="store_true", help='Push config and task data to Gist')
+parser.add_argument('--pull', action="store_true", help='Pull config and task data to Gist')
 
-updateGist(packedJson)
+args = parser.parse_args()
 
-# Load the JSON as JSON Object
-# packedJsonObj = json.loads(packedJson)
-# data_remote = str(packedJsonObj['data']).replace("\\n","\n")
+if args.push:
+    # print('Push')
+    ## Push Config
+    packConfig()
+    data = encryptConfig()
+    packedJson = packJson( data )
+    updateGist(packedJson)
 
-# decryptConfig( 'zulhilmi.ramli@outlook.my', data_remote )
-
-# # Get local config information. Drop ms and ns from epoch value
-# m_time_local = int(str(os.path.getmtime(home + please_config_path)).split('.',1)[0])
-# #print(m_time_local)
-
-# # Get gist, in JSON
-# remoteGistData = getGist()
-# # Get the content, in encrypted state, and convert into json object
-# remoteGistContent = json.loads(remoteGistData['files']['please-sync-data.json']['content'])
-# #print(remoteGistContent)
-# # Get modified date
-# m_time_remote = int(remoteGistContent['modified'])
-# #print(m_time_remote)
-
-# if m_time_local == m_time_remote:
-#     print("Local and Remote list is same")
-# elif m_time_local > m_time_remote:
-#     print("Local list newer than Remote")
-#     # Encrypt local data into ascii armored PGP encryption
-#     r = subprocess.run(args=[ "gpg",
-#                           "--recipient",
-#                           pgp_id,
-#                           "--encrypt",
-#                           "--armor",
-#                           "--output",
-#                           "-",
-#                           home + please_config_path],
-#                   universal_newlines = True,
-#                   stdout = subprocess.PIPE )
-#     #print(r.stdout.replace("\n", "\\n"))
-#     # Build JSON object
-#     config_data = {}
-#     config_data['modified'] = m_time_local
-#     config_data['data'] = r.stdout.replace("\n", "\\n")
-#     json_config_data = json.dumps(config_data)
-#     #print(json_config_data)
-#     updateGist( json_config_data )
-# else:
-#     print("Remote list newer than Local")
-#     # Backup local copy
-#     r = subprocess.run(args=["cp", "-r", home + please_config_path, home + please_config_path + ".bak"])
-#     # Get remote content
-#     remote_config_data = str(remoteGistContent['data']).replace("\\n","\n")
-#     #print(remote_config_data)
-#     # decrypt the data and overwrite local data
-#     echo_r = subprocess.Popen(('echo', remote_config_data), stdout=subprocess.PIPE)
-#     decrypt_r = subprocess.Popen(('gpg', '--decrypt'), stdin=echo_r.stdout, stdout=subprocess.PIPE)
-#     write_r = subprocess.Popen(('tee', home + please_config_path), stdin=decrypt_r.stdout, stdout=subprocess.PIPE)
-#     echo_r.wait()
-#     decrypt_r.wait()
-#     write_r.wait()
-#     print(decrypt_r.stdout)
-
+if args.pull:
+    # print('Pull')
+    ## Pull Config
+    # Get gist, in JSON
+    remoteGistData = getGist()
+    # Get the content, in encrypted state, and convert into json object
+    remoteGistContent = json.loads(remoteGistData['files'][GIST_FILENAME]['content'])
+    # Get remote content
+    remoteConfigData = str(remoteGistContent['data']).replace("\\n","\n")
+    decryptConfig(remoteConfigData)
+    # Unpack
+    unpackConfig()
