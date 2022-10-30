5b5a99051f7a60091dad7cb7d6cbf828d510d5d1
kritikal <zulhilmi.ramli@outlook.my>
2022-10-30 20:22:44 +0800

    Add modified value (#1)
    * WIP
    
    * Pulling will check for modified date and request user  confirmation if local is newer
    
    * Pull and Push will compare remote and local modified timestamp

--- taskwarrior-sync.py
+++ taskwarrior-sync.py
@@ -8,12 +8,14 @@ The sequence for syncing are:
 4. Push
 '''
 
+from datetime import datetime
 import requests
 import json
 import os
 import subprocess
 from pathlib import Path
 import argparse
+import re
 
 ## Check for required variable. Since most of it sensitive, user must set it up
 try:
@@ -41,6 +43,10 @@ TMP = "/tmp/"
 PACKED_CONFIG = "taskConfig.tar.gz"
 PACKED_CONFIG_DECRYPTED = "taskConfig_dec.tar.gz"
 GIST_FILENAME = "task-sync.json"
+BACKLOG_TASK = TASK_FOLDER + "/backlog.data"
+COMPLETED_TASK = TASK_FOLDER + "/completed.data"
+PENDING_TASK = TASK_FOLDER + "/pending.data"
+UNDO_TASK = TASK_FOLDER + "/undo.data"
 
 
 ## Compress file and folder via tar -czf task.tar.gz TASK_CONFIG TASK_FOLDER
@@ -95,11 +101,11 @@ def decryptConfig( input ):
     write_r.wait()
     # print(decrypt_r.stdout)s
 
-def packJson( input ):
+def packJson( input, timestamp ):
     print("Packing config to JSON format...")
     # Build JSON object
     packedData = {}
-    packedData['modified'] = 0
+    packedData['modified'] = timestamp
     packedData['data'] = input.replace("\n", "\\n")
 
     # Build JSON Object
@@ -110,11 +116,13 @@ def packJson( input ):
 
 ## Get gist
 def getGist():
-    print("Pulling config from GIST...")
+    print("Fetching config from GIST...")
     headers = {'Authorization': f'token {GIST_ACCESS_TOKEN}'}
     r = requests.get('https://api.github.com/gists/' + GIST_ID, headers=headers) 
     # print(r.json())
-    return r.json()
+    jsonData = r.json()
+
+    return json.loads(jsonData['files'][GIST_FILENAME]['content'])
 
 ## Update gist
 def updateGist( content ):
@@ -123,6 +131,64 @@ def updateGist( content ):
     headers = {'Authorization': f'token {GIST_ACCESS_TOKEN}'}
     r = requests.patch('https://api.github.com/gists/' + GIST_ID, data=json.dumps({'files':{GIST_FILENAME:{"content":content}}}),headers=headers) 
     #print(r.json())
+
+## Using file modified as indicator more recent file is not reliable as if 
+#   we just did sync, the file modified will be more recent than the one
+#   from Gist. Instead, we check all list, get the last entry each,
+#   get the modified value. Any copy that has more recent modified most
+#   like is the latest one
+def checkVersion():
+    # Check completed, backlog, pending and undo both remote and local
+    # Get the last entry
+    # Get the modified value from both and compare
+    print("Version checked")
+
+# Get last line of each file
+def getLastLine( inputFile ):
+    file = open( inputFile, 'r' )
+    lines = file.readlines()
+    for line in lines:
+        lastLine = line
+    return lastLine
+
+# Only backlog file use proper JSON
+# Return Epoch in integer type
+def getModifiedBacklog( inputJSON ):
+    # Build JSON Object
+    jData = json.loads(inputJSON)
+    # Backlog uses ISO8601 date format, but not quite. Need to append few thing before it compliance
+    modified = jData['modified']
+    modified = modified[:4] + "-" + modified[4:6] + "-" + modified[6:11] + ":" + modified[11:13] + ":" + modified[13:]
+    utc_dt = datetime.strptime( modified, '%Y-%m-%dT%H:%M:%SZ' )
+    timestamp = (utc_dt - datetime(1970, 1, 1)).total_seconds()
+    return int(timestamp)
+
+# Get modified epoch for other file
+def getModified( inputString ):
+    # Build JSON Object
+    output = re.search("modified:\"(.*)\" status", inputString )
+    # print(output[1])
+    return int(output[1])
+
+# Return most recent timestamp
+def getLatestModified():
+    print("Get latest modified timestamp...")
+    # Get each file last line
+    backlogLastLine = getLastLine( BACKLOG_TASK )
+    backlogLatestModified = getModifiedBacklog( backlogLastLine )
+    # print(backlogLatestModified)
+
+    completedLastLine = getLastLine( COMPLETED_TASK )
+    completedLatestModified = getModified( completedLastLine )
+    # print(completedLatestModified)
+
+    pendingLastLine = getLastLine( PENDING_TASK )
+    pendingLatestModified = getModified( pendingLastLine )
+    # print(pendingLatestModified)
+    
+    allModified = [ backlogLatestModified, completedLatestModified, pendingLatestModified]
+
+    return max(allModified)
 
 
 parser = argparse.ArgumentParser(description='Simple Task Warrior task sync. Uses Github as storage and PGP as encryption')
@@ -134,20 +200,66 @@ args = parser.parse_args()
 if args.push:
     # print('Push')
     ## Push Config
-    packConfig()
-    data = encryptConfig()
-    packedJson = packJson( data )
-    updateGist(packedJson)
+    confirmPush = False
+    # Check local version vs remote
+    remoteGistContent = getGist()
+    # Get local data timestamp
+    localModified = getLatestModified()
+    remoteModified = int(remoteGistContent['modified'])
+    print("Comparing local and remote config...")
+    if remoteModified > localModified:
+        # Remote config is newer. Pushing may cause data loss
+        remoteTime = datetime.fromtimestamp(remoteModified)
+        localTime = datetime.fromtimestamp(localModified)
+        userInput = input("Remote copy is newer than local. Proceed pushing? [y/N]: ")
+        if userInput == 'y':
+            confirmPush = True
+        else:
+            print("Push canceled")
+    else:
+        # If local is newer, push the config
+        print("No conflict...")
+        confirmPush = True
+
+    if confirmPush:
+        packConfig()
+        data = encryptConfig()
+        packedJson = packJson( data, localModified )
+        updateGist(packedJson)
+
 
 if args.pull:
     # print('Pull')
     ## Pull Config
+    confirmPull = False
     # Get gist, in JSON
-    remoteGistData = getGist()
-    # Get the content, in encrypted state, and convert into json object
-    remoteGistContent = json.loads(remoteGistData['files'][GIST_FILENAME]['content'])
-    # Get remote content
-    remoteConfigData = str(remoteGistContent['data']).replace("\\n","\n")
-    decryptConfig(remoteConfigData)
-    # Unpack
-    unpackConfig()
+    remoteGistContent = getGist()
+    # Get local data timestamp
+    localModified = getLatestModified()
+    remoteModified = int(remoteGistContent['modified'])
+    print("Comparing local and remote config...")
+    if localModified > remoteModified:
+        # Local newer than remote, ask user to continue or not
+        localTime = datetime.fromtimestamp(localModified)
+        remoteTime = datetime.fromtimestamp(remoteModified)
+        print("Local copy timestamp: " + str(localTime))
+        print("Remote copy timestamp: " + str(remoteTime))
+        userInput = input("Local copy is newer than remote. Proceed pulling? [y/N]: ")
+        if userInput == 'y':
+            confirmPull = True
+        else:
+            print("Pull canceled")
+    else:
+        print("No conflict...")
+        confirmPull = True
+    
+    if confirmPull:
+        # Get remote content
+        remoteConfigData = str(remoteGistContent['data']).replace("\\n","\n")
+        decryptConfig(remoteConfigData)
+        # Unpack
+        unpackConfig()
+        print("Pull completed")
+
+# maxMod = getLatestModified()
+# print(maxMod)
